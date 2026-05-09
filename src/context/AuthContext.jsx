f224ea798a0344707e84197ab914e9aff360d4e8
quanzz123 <nguyenquandc37@gmail.com>
2026-05-09 11:36:16 +0700

    Added Auth

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,19 +1,63 @@
-import { createContext, useState } from "react";
+import { createContext, use, useState } from "react";
 
 
 export const AuthContext = createContext();
 
 export default function AuthProvider({children}) {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(
+        localStorage.getItem("currentUserEmail") // nếu người dùng đã đăng nhập thì dùng email có trong localstorage
+        ? {email: localStorage.getItem("currentUserEmail")}
+        : null // ngược lại chưa có thì user = null
+    );
 
     function signup(email, password) {
         const users = JSON.parse(localStorage.getItem("users") || "[]");
+        
+        //kiểm tra email mới nhập dã tồn tại hay chưa
+        if(users.find((u) => u.email === email)) {
+            return {success: false, error: "Email already exits"};
+        }
+        
 
         const newUser = {email, password};
         users.push(newUser);
         localStorage.setItem("users", JSON.stringify(users));
+        // lưu vào storage một email hiện tại 
+        localStorage.setItem("currentUserEmail", email);
         setUser(newUser);
+
+        // khi người dung đăng kí thì setUser là email, cập nhật state user thành một object
+        setUser({email})
+        return {success: true}
+    }
+    function login(email, password) {
+        // lấy ra các user được lưu trong localstorage
+
+        const users = JSON.parse(localStorage.getItem("users") || "[]");
+        // tìm user vừa đăng nhập
+        const user = users.find((u) => u.email === email && u.password === password);
+
+        // kiểm tra không khớp thì báo lõi
+        if(!user) {
+            return {
+                success: false,
+                error: "Invalid email or password"
+
+            }
+        }
+        //nếu đúng thì set trong localstorage currentUserEmail
+        localStorage.setItem("currentUserEmail", email);
+        setUser({email});
+        return {
+            success: true,
+            
+        }
+
+    }
+    function logout() {
+        localStorage.removeItem("currentUserEmail");
+        setUser(null);
     }
 
-    return <AuthContext.Provider value={{signup}}>{children}</AuthContext.Provider>
+    return <AuthContext.Provider value={{signup, user,logout, login}}>{children}</AuthContext.Provider>
 }
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,20 +1,44 @@
 import { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { AuthContext } from '../context/AuthContext';
+import { Navigate, useNavigate } from 'react-router-dom';
 export default function Auth() {
-    const [mode, setMode] = useState('login');
+    const [mode, setMode] = useState('signup');
+    const [error, setError] = useState(null)
     const { register, handleSubmit , formState: { errors }} = useForm();
-    const {signup} = useContext(AuthContext);
+    const {signup, user,logout,login} = useContext(AuthContext);
+    const navigate = useNavigate();
 
+    // hàm khi ấn submit thì form gửi lên data gồm pass và email
     function onSubmit(data) {
-        signup(data.email, data.password);
+        setError(null)
+        let result;
+        //kiểm tra mode(chế độ)
+        //nếu mode === signup
+        if(mode === "signup") {
+
+            result = signup(data.email, data.password); // đăng kí
+        }  else {
+            result = login(data.email, data.password); // đăng nhập
+        }
+        // trong AuthContext các hàm sẽ trả success là true or false, và biến error chưa cảnh báo
+        if(result.success) {
+            navigate("/");
+        } else {
+            setError(result.error)
+        }
+        console.log(result)
     }
     return (
         <div className="page">
             <div className="container">
+                
                 <div className="auth-container">
                     <h1 className="page-title">{mode === 'signup' ? 'Sign Up' : 'Login'}</h1>
+                    {user && <p>User login in {user.email}</p>}
+                    <button onClick={() => logout()}>logout</button>
                     <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
+                        {error && <div className="error-message">{error}</div>}
                         <div className="form-group">
                             <label className="form-label" htmlFor="email">Email</label>
                             <input className="form-input" type="email" id="email" {...register('email',{required: "Email is required"})} />
